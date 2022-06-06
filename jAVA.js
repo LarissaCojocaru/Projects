da4fe5a373c5e3970fd9dc8e2e9380bbb3065a3a
LarissaCojocaru <106817613+LarissaCojocaru@users.noreply.github.com>
2022-06-06 14:57:36 +0300

    Add files via upload

---
+++ jAVA.js
@@ -0,0 +1,10 @@
+
+var button =document.querySelector(".button");
+console.log("button");
+var image = document.querySelector(".img-lucrare");
+button.addEventListener("click" , function){
+img.style.width= "400px"
+console. log ("you clicked me")
+
+ } ,false)
+ 
